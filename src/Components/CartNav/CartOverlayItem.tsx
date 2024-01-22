59039af3b36a19c80264e468352c22b1dca04db3
melchanatan <chanatan@outlook.com>
2024-01-22 13:12:12 +0700

    fix: update CartEmptyIcon

--- src/Components/CartNav/CartOverlayItem.tsx
+++ src/Components/CartNav/CartOverlayItem.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import CloseIcon from './assets/ProfileIcon'
 import { CartItem } from '@/types'
 import { useSelector } from 'react-redux'
+import { InteractiveOverlay } from '../InteractiveOverlay'
 
-const CartOverlayItem = ({itemId, item}: {itemId: string, item: CartItem[0]}) => {
+const  CartOverlayItem = ({itemId, item}: {itemId: string, item: CartItem[0]}) => {
   const itemDetail = useSelector((state: any) => state.products.products[itemId])
 
   return (
     <li className='solid-border rounded-[15px] flex gap-4 items-center pr-4'>
-        <div className="bg-[url('/banana-wall.png')] w-[6rem] h-[6rem] bg-cover rounded-[11px]"></div>
+        <div className="bg-[url('/banana-wall.png')] w-[6rem] h-[6rem] bg-cover rounded-[11px]">
+
+        </div>
         <div>
           <h5 className='font-semibold'>{itemDetail.productName}</h5>
           <p className='text-gray-500'>{itemDetail.price} $</p>
